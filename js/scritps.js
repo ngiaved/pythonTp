463f35c979f153ff1d7173e23faea7f9686378b3
Maria Martinez <meris.102012@gmail.com>
2022-10-31 01:57:07 -0300

    Ultimos Detalles

--- js/scritps.js
+++ js/scritps.js
@@ -38,3 +38,53 @@
    } )
 
 }, 2000);
+
+
+function validarEnviar() {
+
+    //Función para validar el envío de datos del formulario
+    if (document.validarForm.nombre.value.length < 3) {
+        alert("Escriba su nombre completo por favor")
+        document.validarForm.nombre.focus()
+        return 0;
+    }
+    edad = document.validarForm.edad.value
+    edad = validarEntero(edad)
+    document.validarForm.edad.value = edad
+    if (edad == "") {
+        alert("Introducir edad como número entero")
+        document.validarForm.edad.focus()
+        return 0;
+    }
+    tel = document.validarForm.tel.value
+    tel = validarEntero(tel)
+    document.validarForm.tel.value = tel
+    if (tel == "") {
+        alert("Introducir teléfono")
+        document.validarForm.tel.focus()
+        return 0;
+    }
+    if (document.validarForm.tipoConsulta.selectedIndex == 0) {
+        alert("Debe seleccionar su tipo de consulta por favor")
+        document.validarForm.tipoConsulta.focus()
+        return 0;
+    }
+    if (document.validarForm.mensaje.value == "") {
+        alert("Ingrese su consulta por favor")
+        document.validarForm.mensaje.focus()
+        return 0;
+    }
+    //se envía el formulario
+    alert("Muchas gracias por enviar su consulta")
+    document.validarForm.submit();
+}
+function validarEntero(valor) {
+    valor = parseInt(valor)
+    if (isNaN(valor)) {
+        return ""
+    } else {
+        return valor
+    }
+}
+
+
